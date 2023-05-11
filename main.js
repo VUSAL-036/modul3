5b2557035d5247b67e0cf2f125e12b3c1cd573e5
ElshanNadiev13 <platin618@gmail.com>
2023-05-11 16:41:32 +0400

    fatality

--- main.js
+++ main.js
@@ -1,97 +1,97 @@
-const url = 'https://api.exchanherate.host/latest';
+const url = 'https://api.exchangerate.host/latest';
 
-let currentCurr = 'RUB',exchangeCurr = 'USD'
+
+let currentCurr = 'RUB', exchangeCurr = 'USD';
 let currVal = 1;
 
-const inpleft = document.querySelector('.i1');
-const  inpRight = document.querySelector('.i12');
+const inpLeft = document.querySelector('#sol-i');
+const inpRight = document.querySelector('#sag-i');
 
-const currLeft = document.querySelector('.yoruldum1');
-const currRight = document.querySelector('.yoruldum2');
+const currLeft = document.querySelector('.sol-p');
+const currRight = document.querySelector('.sag-p');
 
-const btnsLeft = document.querySelector('.duymeb').children;
-for(let i=0; i<btnsLeft.length; i++) {
-    btnsLeft[i].addEventListener('click',changeExchangeCurrency);
+const btnLeft = document.querySelectorAll('.btns1 .b');
+for (let i = 0; i < btnLeft.length; i++) {
+    btnLeft[i].addEventListener('click', changeCurrentCurrency);
 }
 
-
-const btnsRight = document.querySelector('.duymeb2').children;
-for(let i=0; i<btnsRight.length; i++) {
-    btnsRight[i].addEventListener(click),changeExchangeCurrency;
+const btnRight = document.querySelectorAll('.btns2 .b');
+for (let i = 0; i < btnRight.length; i++) {
+    btnRight[i].addEventListener('click', changeExchangeCurrency);
 }
 
-async function loadCurrency(a,b) {
-    if(a===b)return 1;
-    const query =`?base=${a}&symbols=${b};`
+async function loadCurrency(a, b) {
+    if (a === b) return 1;
+    const query = `?base=${a}&symbols=${b}`;
     const res = await fetch(url + query);
     const data = await res.json();
-        return data.rates[b];
-    }
-    
-    function getdata(c, e, l, r) {
-        loadCurrency(c, e)
+    return data.rates[b];
+}
+
+function getData(c, e, l, r) {
+    loadCurrency(c, e)
         .then(data => {
             currVal = data.toFixed(4);
             const val = parseFloat(l.value);
-            r.value =  (val * currVal).toFixed(4);
+            r.value = (val * currVal).toFixed(4);
             currLeft.innerText = `1 ${c} = ${currVal} ${e}`;
-            const excVal = (1/currVal).toFixed(4);
-            currRight.innerText = `1${e} = ${excVal} {c};`
+            const excVal = (1 / currVal).toFixed(4);
+            currRight.innerText = `1 ${e} = ${excVal} ${c}`;
         })
         .catch(e => alert('Network error: error while getting data'));
-    }
-
-    function changeExchangeCurrency(e) {
-        const btn = e.target;
-        for(let i=0; i<btnsLeft.length; i++) {
-            btnsLeft[i].classList.remove('curr-active');
-        }
-        btn.classList.add('curr-active');
-        currentCurr = btn.innerText.toUpperCase();
-        getdata(currentCurr, exchangeCurr, inpleft, inpRight);
-    }
-
-    function changeExchangeCurrency(e) {
-        const btn =e.target;
-        for(let i=0; i<btnsRight.length; i++){
-            btnsRight[i].classList.remove('curr-active');
-        }
-        btn.classList.add('curr-active');
-        exchangeCurr = btn.innerText.toUpperCase();
-        getdata(currentCurr, exchangeCurr, inpleft, inpRight);
-    }
-
-    inpleft.addEventListener('input', (e) => {
-        if(e.target.value === '') {
-            e.target.value = 0;
-        }
-        e.target.value = e.target.value.replaceAll(',','.');
-        getdata(currentCurr, exchangeCurr, inpleft, inpRight);
-    });
-
-    inpRight.addEventListener('input', (e) => {
-        if(e.target.value === '') {
-            e.target.value === 0;
-        }
-        e.target.value=e.target.value.replaceAll(',','.');
-        getdata(exchangeCurr, currentCurr, inpLeft, inpRight);
-    });
-
-    inpleft.addEventListener('keydown', acceptNumber);
-    inpRight.addEventListener('keydown', acceptNumber);
-
-    function acceptNumber(e) {
-        if( !( (e.key === 'Backspace')|| (e.key === 'Delete') ||
-        (('0'<=e.key) && (e.key<='9'))||
-        (e.key === '.') || (e.key ===',')
-        )){
-    e.preventDefault();
 }
-if((e.key === ',') || (e.key === ',')) {
-    if(e.target.value.indexOf(',') >= 0) {
+
+function changeCurrentCurrency(e) {
+    const btn = e.target;
+    for (let i = 0; i < btnLeft.length; i++) {
+        btnLeft[i].classList.remove('b-active');
+    }
+    btn.classList.add('b-active');
+    currentCurr = btn.innerText.toUpperCase();
+    getData(currentCurr, exchangeCurr, inpLeft, inpRight);
+}
+
+function changeExchangeCurrency(e) {
+    const btn = e.target;
+    for (let i = 0; i < btnRight.length; i++) {
+        btnRight[i].classList.remove('b-active');
+    }
+    btn.classList.add('b-active');
+    exchangeCurr = btn.innerText.toUpperCase();
+    getData(currentCurr, exchangeCurr, inpLeft, inpRight);
+}
+
+inpLeft.addEventListener('input', (e) => {
+    if (e.target.value === '') {
+        e.target.value = 0;
+    }
+    e.target.value = e.target.value.replaceAll(',', '.');
+    getData(currentCurr, exchangeCurr, inpLeft, inpRight);
+});
+
+inpRight.addEventListener('input', (e) => {
+    if (e.target.value === '') {
+        e.target.value = 0;
+    }
+    e.target.value = e.target.value.replaceAll(',', ',');
+    getData(exchangeCurr, currentCurr, inpRight, inpLeft);
+});
+
+inpLeft.addEventListener('keydown', acceptNumber);
+inpRight.addEventListener('keydown', acceptNumber);
+
+function acceptNumber(e) {
+    if( ! ( (e.key === 'Backspace') || (e.key === 'Delete') ||
+    ( ('0' <= e.key) && (e.key <= '9') ) ||
+    (e.key === '.') || (e.key === '.') 
+    ) ){
         e.preventDefault();
     }
-}
+    if ((e.key === ',') || (e.key === '.')) {
+        if (e.target.value.indexOf('.') >= 0) {
+            e.preventDefault();
+        }
     }
+}
 
-getData{currentCurr, exchangeCurr, inpleft,inpRight};
+getData(currentCurr, exchangeCurr, inpLeft, inpRight);
